cf593e6e90f9ca7e82c00ff0c055327a4d571e70
Petros <petrospapantonopoulos99@gmail.com>
2022-05-19 15:55:14 +0300

    Implemented util functions for getting recommendations from trained model svd

---
+++ trainModels.py
@@ -0,0 +1,78 @@
+import io
+import os
+from pymongo import MongoClient
+from surprise import SVD, KNNBaseline, dump, Reader, Dataset
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.MovienderDB
+
+
+def export_data_file():
+    filePath = "C:\\Users\\Petros\\PycharmProjects\\LightFM_examples\\movielens\\dataset\\ml-1m\\ratings.dat"
+    ratings = []
+
+    with io.open(filePath, 'r', encoding='ISO-8859-1') as f:
+        for line in f:
+            line_split = line.split('::')
+            ratings.append(line_split)
+
+    cursor = list(db.Ratings.find())
+
+    for user in cursor:
+        for movie_id in user["ratings"].keys():
+            ratings.append([user['uid'], movie_id, user["ratings"][movie_id], "000000000"])
+
+    with open("ratings.dat", 'w') as f:
+        for rating in ratings:
+            uid = rating[0]
+            movie_id = rating[1]
+            rating_value = int(rating[2])
+            timestamp = rating[3].strip()
+            f.write(f"{uid}::{movie_id}::{rating_value}::{timestamp}\n")
+
+
+def load_custom_dataset():
+    file_path_large = "C:\\Users\\Petros\\PycharmProjects\\Moviender-API\\ratings.dat"
+
+    reader_large = Reader(line_format='user item rating timestamp', sep='::')
+
+    return Dataset.load_from_file(file_path=file_path_large, reader=reader_large)
+
+
+def train_svd():
+    data = load_custom_dataset()
+
+    trainset = data.build_full_trainset()
+
+    algo = SVD(n_factors=40, n_epochs=20, lr_all=0.005)
+    algo.fit(trainset)
+
+    # dump trained algorithm
+    file_name = os.path.expanduser('TrainedModels\\trainedSVDAlgo.model')
+    dump.dump(file_name=file_name, algo=algo)
+
+
+def train_knn():
+    # load custom dataset
+    data = load_custom_dataset()
+    trainset = data.build_full_trainset()
+
+    # First, train the algorithm to compute the similarities between items
+    sim_options = {'name': 'pearson_baseline', 'user_based': False}
+    algo = KNNBaseline(sim_options=sim_options)
+    algo.fit(trainset)
+
+    # dump trained algorithm
+    file_name = os.path.expanduser('TrainedModels\\trainedKNNBaseline.model')
+    dump.dump(file_name=file_name, algo=algo)
+
+
+def main():
+    export_data_file()
+    train_svd()
+    train_knn()
+    print("Training done!")
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from enum import IntEnum
 from firebase_admin import messaging
+from surprise import dump
 
 
 class User(BaseModel):
@@ -28,6 +29,7 @@ class State(IntEnum):
     PENDING = 1
     REQUEST = 2
     FRIEND = 3
+    SESSION = 4
 
 
 class Status(IntEnum):
@@ -38,6 +40,11 @@ class Status(IntEnum):
     ACCEPT_REQUEST = 1
     DECLINE_REQUEST = 0
 
+
+class SessionStatus(IntEnum):
+    WAITING_FOR_VOTES = 0
+    SUCCESSFUL_FINISH = 1
+    FAILED_FINISH = -1
 
 
 def convert_user_ratings_to_json(user_ratings: UserRatings):
@@ -74,3 +81,50 @@ def send_friend_request_notification(username: str, token: str):
     response = messaging.send(message)
     # Response is a message ID string.
     print('Successfully sent message:', response)
+
+
+def get_recommendation(uid: str, friend_uid: str, genres_ids, db):
+    list_of_movies = fetch_movies(uid, friend_uid, genres_ids, db)
+    result_tuple = get_final_list(uid, friend_uid, list_of_movies)
+    return [recommendation[0] for recommendation in result_tuple]
+
+
+def fetch_movies(uid: str, friend_uid: str, genres_ids, db):
+    firstUser = list(list(db.Ratings.find({"uid": uid}))[0]["ratings"].keys())
+    secondUser = list(list(db.Ratings.find({"uid": friend_uid}))[0]["ratings"].keys())
+
+    mergedMoviesList = list(set(firstUser + secondUser))
+
+    if genres_ids:
+        match_options = {"$and": [
+            {"genre_ids": {"$in": genres_ids}},
+            {"movielens_id": {"$nin": mergedMoviesList}}]}
+    else:
+        match_options = {"movielens_id": {"$nin": mergedMoviesList}}
+
+    pipeline = [
+        {"$match": match_options},
+        {"$project": {"_id": 0, "movielens_id": 1}}
+    ]
+
+    cursor = db.Movies.aggregate(pipeline)
+    cursor = list(cursor)
+    return [movie_id["movielens_id"] for movie_id in cursor]
+
+
+def get_final_list(uid: str, friend_uid: str, list_of_movies):
+    # load a tuple with (prediction, trained-algorithm)
+    algo = dump.load("TrainedModels/trainedSVDAlgo.model")
+    # get just the algorithm
+    algo = algo[1]
+
+    user_combined_predictions = []
+    for movie_id in list_of_movies:
+        user_pred = algo.predict(uid, movie_id)
+        friend_user_pred = algo.predict(friend_uid, movie_id)
+        user_combined_predictions.append((movie_id, (user_pred.est + friend_user_pred.est) / 2.0))
+
+    user_combined_predictions.sort(key=lambda x: x[1], reverse=True)
+    if len(user_combined_predictions) > 50:
+        user_combined_predictions = user_combined_predictions[0:50]
+    return user_combined_predictions
