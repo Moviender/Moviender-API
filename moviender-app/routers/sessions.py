6628b6dee0c26ea2a07c7ec328e823df9eae4876
Petros <petrospapantonopoulos99@gmail.com>
2022-06-01 09:36:07 +0300

    Chnage find to find_one in init_friends_session

--- moviender-app/routers/sessions.py
+++ moviender-app/routers/sessions.py
@@ -50,35 +50,35 @@ async def get_session_result_list(session_id: str):
 
 @router.post("/session/{uid}", tags=["sessions"])
 async def init_friends_session(uid: str, body: SessionRequestBody):
+
     # Check if user have an opened session with current friend
-    inSession = list(db.Users.find({"uid": uid, f"friend_list.{body.friend_uid}": 3})) == []
+    inSession = db.Users.find_one({"uid": uid, f"friend_list.{body.friend_uid}": 3})
 
-    if not inSession:
-
-        top_n_recommendation = get_recommendation(uid, body.friend_uid, body.genres_ids)
-        result = db.Sessions.insert_one({
-            "users_in_session": [uid, body.friend_uid],
-            "users_session_info": {uid: {"state": SessionUserStatus.VOTING, "voted_movies": []},
-                                   body.friend_uid: {"state": SessionUserStatus.VOTING, "voted_movies": []}},
-            "results": [],
-            "users_voted": 0,
-            "recommendations": top_n_recommendation,
-            "is_active": True,
-            "state": SessionStatus.WAITING_FOR_VOTES
-        })
-
-        db.Users.update_one(
-            {"uid": uid},
-            {"$set": {f"friend_list.{body.friend_uid}": State.SESSION}}
-        )
-        db.Users.update_one(
-            {"uid": body.friend_uid},
-            {"$set": {f"friend_list.{uid}": State.SESSION}}
-        )
-
-        return {"session_id": str(result.inserted_id)}
-    else:
+    if inSession is None:
         return None
+
+    top_n_recommendation = get_recommendation(uid, body.friend_uid, body.genres_ids)
+    result = db.Sessions.insert_one({
+        "users_in_session": [uid, body.friend_uid],
+        "users_session_info": {uid: {"state": SessionUserStatus.VOTING, "voted_movies": []},
+                               body.friend_uid: {"state": SessionUserStatus.VOTING, "voted_movies": []}},
+        "results": [],
+        "users_voted": 0,
+        "recommendations": top_n_recommendation,
+        "is_active": True,
+        "state": SessionStatus.WAITING_FOR_VOTES
+    })
+
+    db.Users.update_one(
+        {"uid": uid},
+        {"$set": {f"friend_list.{body.friend_uid}": State.SESSION}}
+    )
+    db.Users.update_one(
+        {"uid": body.friend_uid},
+        {"$set": {f"friend_list.{uid}": State.SESSION}}
+    )
+
+    return {"session_id": str(result.inserted_id)}
 
 
 @router.post("/vote_in_session/{session_id}", tags=["sessions"])
